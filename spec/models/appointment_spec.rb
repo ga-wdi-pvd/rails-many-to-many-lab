f9ceab7e95fadcd30ec2c8416580849d83bfbd40
Danny Kirschner <rkirschner1377@gmail.com>
2016-12-22 16:42:39 -0500

    Added the appointment model and migrated db. Starting appointment specs with rspec.

---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Appointment, type: :model do
+  describe "associations" do
+    it "has one doctor" do
+      pending
+    end
+
+    it "has one patient" do
+      pending
+    end
+  end
+
+end
