862e5b27591e273aafc7d6ec59786513cd8fce5d
Danny Kirschner <rkirschner1377@gmail.com>
2016-12-22 13:31:00 -0500

    deleting test dir

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  describe "Appointment" do
-    let(:appointment){Appointment.new}
+  let(:appointment){Appointment.new}
 
-    describe "associations" do
-      let(:patient) { Patient.new }
-      let(:doctor) { Doctor.new }
-      before do
-        appointment.patient = patient
-        appointment.doctor = doctor
-      end
-      it "belongs to patient" do
-        expect(appointment.patient).to eq patient
-      end
-      it "belongs to doctor" do
-        expect(appointment.doctor).to eq doctor
-      end
+  describe "associations" do
+    let(:patient) { Patient.new }
+    let(:doctor) { Doctor.new }
+    before do
+      appointment.patient = patient
+      appointment.doctor = doctor
+    end
+    it "belongs to patient" do
+      expect(appointment.patient).to eq patient
+    end
+    it "belongs to doctor" do
+      expect(appointment.doctor).to eq doctor
     end
   end
 end
